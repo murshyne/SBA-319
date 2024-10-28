4182d1887392faec0026c8cb1dd514e97b41ca9f
Lwrnc.E.U. <61216539+murshyne@users.noreply.github.com>
2024-10-27 22:24:54 -0400

    Add files via upload

--- data/data.mjs
+++ data/data.mjs
@@ -14,25 +14,44 @@ export const recipes = [
   {
     name: "Chicken Curry",
     userId: "user2",
-    ingredients: ["chicken", "curry powder", "coconut milk", "onion", "garlic"],
+    ingredients: [
+      "chicken", 
+      "curry powder", 
+      "coconut milk", 
+      "onion", 
+      "garlic"],
     year: 2023,
   },
   {
     name: "Vegetable Stir Fry",
-    userId: "user1",
-    ingredients: ["mixed vegetables", "soy sauce", "ginger", "garlic"],
+    userId: "user3",
+    ingredients: [
+      "mixed vegetables", 
+      "soy sauce", 
+      "ginger", 
+      "garlic"],
     year: 2023,
   },
   {
     name: "Beef Tacos",
-    userId: "user3",
-    ingredients: ["ground beef", "taco shells", "lettuce", "cheese", "salsa"],
+    userId: "user4",
+    ingredients: [
+      "ground beef", 
+      "taco shells", 
+      "lettuce", 
+      "cheese", 
+      "salsa"],
     year: 2023,
   },
   {
     name: "Chocolate Cake",
-    userId: "user2",
-    ingredients: ["flour", "cocoa powder", "sugar", "eggs", "butter"],
+    userId: "user5",
+    ingredients: [
+      "flour", 
+      "cocoa powder", 
+      "sugar", 
+      "eggs", 
+      "butter"],
     year: 2023,
   },
 ];
